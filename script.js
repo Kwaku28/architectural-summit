b854de3ab1424979e29cd9bbb973576067332935
Lawrence Amoafo <rekenburg777@gmail.com>
2023-03-16 18:17:05 +0000

    Configure to toggle feature speakers section in mobile view

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ const guests = [
     id: '5',
     speakerName: 'Naser Raed Fakhouri',
     title: 'Architect | Bainona Engineering Consultancy',
-    experience: 'with a Bachelor of Architecture degree from the American University of Sharjah. Fakhouri’s work attempts to bridge the gap between architecture, locality, context, and vernacularity.',
+    experience: 'With a Bachelor of Architecture degree from the American University of Sharjah. Fakhouri’s work attempts to bridge the gap between architecture, locality, context, and vernacularity.',
     faceshot: './Images/Naser.png',
   },
   {
@@ -53,59 +53,59 @@ const guests = [
     title: 'Director | RSP',
     experience: 'Over 17 years of professional experience in Asia, Australia, Middle East and Europe. His predilection with architecture is one that exerts a comprehensive knowledge base spanning from conceptual design through to project delivery.',
     faceshot: './Images/Ralf.png',
-  }
+  },
 ];
+
+const openMore = document.querySelector('.more');
+const closeLess = document.querySelector('.less');
 
 const loadGuests = () => {
   const guestSpeakers = document.querySelector('.guest-speakers');
   guestSpeakers.innerHTML = '';
-  if (window.innerWidth < 768){
-    guests.forEach((guest) => {
-      const li = document.createElement('li');
-      if (guest.id === '1' || guest.id === '2'){
-        li.className = 'guest';
-      }else {
-        li.style.display = 'none'
-      }
-  
-      li.innerHTML = `
-      <div class="faceshot">
-        <img src="${guest.faceshot}" alt="speaker">
-      </div>
-      <div class="resume">
-        <h4 class="name">${guest.speakerName}</h4>
-        <h5 class="title">${guest.title}</h5>
-        <p class="experience">${guest.experience}</p>
-      </div>
-      `;
-  
-      guestSpeakers.appendChild(li);
-    });
-  }else {
-    guests.forEach((guest) => {
-      const li = document.createElement('li');
-      li.classList = 'guest'
-  
-      li.innerHTML = `
-      <div class="faceshot">
-        <img src="${guest.faceshot}" alt="speaker">
-      </div>
-      <div class="resume">
-        <h4 class="name">${guest.speakerName}</h4>
-        <h5 class="title">${guest.title}</h5>
-        <p class="experience">${guest.experience}</p>
-      </div>
-      `;
-  
-      guestSpeakers.appendChild(li);
-    });
-    li.className = 'guest'
-    li.style.display = 'contents'
-  }
+  guests.forEach((guest) => {
+    const li = document.createElement('li');
+    if (guest.id === '1' || guest.id === '2') {
+      li.className = 'guest';
+    } else {
+      li.className = 'guest';
+      li.classList.toggle('active');
+    }
+
+    li.innerHTML = `
+    <div class="faceshot">
+      <img src="${guest.faceshot}" alt="headshot of ${guest.speakerName}">
+    </div>
+    <div class="resume">
+      <h4 class="name">${guest.speakerName}</h4>
+      <h5 class="title">${guest.title}</h5>
+      <span class="line-secondary></span>
+      <p class="experience">${guest.experience}</p>
+    </div>
+    `;
+
+    guestSpeakers.appendChild(li);
+  });
 };
 
 window.onload = () => {
   loadGuests();
 };
 
-const moreOrLess = document.querySelector('.toggle')
+openMore.addEventListener('click', () => {
+  const li = document.getElementsByClassName('guest');
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].setAttribute('class', 'guest');
+  }
+  openMore.classList.add('active');
+  closeLess.classList.add('active');
+});
+
+closeLess.addEventListener('click', () => {
+  const li = document.getElementsByClassName('guest');
+  for (let i = 2; i < li.length; i += 1) {
+    li[i].setAttribute('class', 'guest active');
+  }
+
+  closeLess.classList.remove('active');
+  openMore.classList.remove('active');
+});
